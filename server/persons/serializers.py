1e684622d4e0132dc451a0f0bb92db83444faef0
thierry.tajes <thierry.tajes@nexxera.com>
2022-10-14 20:05:51 -0400

    feat(persons): add test for already name exists

--- server/persons/serializers.py
+++ server/persons/serializers.py
@@ -13,3 +13,6 @@ class PersonSerializer(serializers.ModelSerializer):
     def validate_name(self, value):
         if not value:
             raise serializers.ValidationError("Empty Name")
+        if PersonModel.objects.filter(name=value).exists():
+            raise serializers.ValidationError("Name Already Exists")
+        return value
--- server/persons/tests/test_person_serializer.py
+++ server/persons/tests/test_person_serializer.py
@@ -4,10 +4,22 @@ from rest_framework.exceptions import ValidationError
 
 
 class TestPersonSerializer(TestCase):
-    def test_shoudnt_able_to_add_new_person_with_empty_name(self):
+    def test_shoudnt_be_able_to_add_new_person_with_empty_name(self):
         data = {
             "name": "",
         }
         serializer = PersonSerializer(data=data)
         with self.assertRaises(ValidationError):
             serializer.is_valid(raise_exception=True)
+
+    def test_shouldnt_be_possible_to_add_a_new_person_with_an_existing_name(self):
+        data = {
+            "name": "any name",
+        }
+        serializer = PersonSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+        with self.assertRaises(ValidationError):
+            serializer = PersonSerializer(data=data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
