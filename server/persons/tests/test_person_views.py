ecf6e819cd74e034e0fbce8c36df4dfc1a6dd4ea
thierry.tajes <thierry.tajes@nexxera.com>
2022-10-14 20:25:18 -0400

    feat(persons): add views test for new person with valid name

--- server/persons/tests/test_person_views.py
+++ server/persons/tests/test_person_views.py
@@ -21,3 +21,10 @@ class TestPersonView(TestCase):
         self.client.post("/persons/", data, format="json")
         response = self.client.post("/persons/", data, format="json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_should_be_able_to_add_new_person_with_valid_name(self):
+        data = {
+            "name": "any name",
+        }
+        response = self.client.post("/persons/", data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
